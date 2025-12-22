adb9482f5053257e163494e9c99bd9f026e77335
Nabeel Ahsan <nabeel.cse15.jkkniu@gmail.com>
2025-12-23 00:40:56 +0600

    added the header

--- nebsit-assessment/src/App.jsx
+++ nebsit-assessment/src/App.jsx
@@ -1,11 +1,15 @@
 import Sidebar from './components/Sidebar'
+import Header from './components/Header'
 
 function App() {
   return (
     <div style={{ display: 'flex', minHeight: '100vh', backgroundColor: 'white' }}>
       <Sidebar />
-      <main style={{ flex: 1 }}>
-      </main>
+      <div style={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
+        <Header />
+        <main style={{ flex: 1, backgroundColor: '#FAFAFA' }}>
+        </main>
+      </div>
     </div>
   )
 }
